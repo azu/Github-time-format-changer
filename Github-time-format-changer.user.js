260dcaa4faa720254b014af621f95fbb501ae60e
azu <azuciao@gmail.com>
2014-05-18 13:12:02 +0900

    initial commit

---
+++ Github-time-format-changer.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @id             github.com-ff599db1-47d8-b14f-83b4-3e345f6d67e3@http://efcl.info/
+// @name           Github-time-format-changer
+// @version        1.0
+// @namespace      http://efcl.info/
+// @author         azu
+// @license        MIT
+// @description    Change Github time format.
+// @include        https://github.com/*
+// @run-at         document-end
+// @require        https://cdn.jsdelivr.net/momentjs/2.6.0/moment.min.js
+// ==/UserScript==
+
+var $ = unsafeWindow.$;
+var toArray = Function.prototype.call.bind(Array.prototype.slice);
+var relative = /ago/i;
+function _update(body) {
+    var times = body.getElementsByTagName("time");
+    toArray(times).forEach(function (timeElement) {
+        if (!relative.test(timeElement.textContent)) {
+            timeElement.textContent = moment(timeElement.getAttribute("datetime")).fromNow();
+        }
+    });
+}
+function update(body) {
+    requestAnimationFrame(function () {
+        _update(body);
+    });
+}
+$(document).on('pjax:end', function pjaxEnd() {
+    update(document.body);
+});
+var addFilterHandler = function (evt) {
+    var node = evt.target;
+    update(node);
+};
+document.body.addEventListener('AutoPagerize_DOMNodeInserted', addFilterHandler, false);
+// MAIN =
+update(document.body);
